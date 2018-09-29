cdfa929671b6fa0e2178dd878b4214248163394d
SantiagoPazos925 <santiagopazos925@gmail.com>
2018-09-29 12:23:29 -0300

    nuevo repositorio proyecto

---
+++ TpFinal/destacados.php
@@ -0,0 +1,96 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12 contenedorDestacados">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="row portadasEstrenos">
+              <div class="col-6 uno">
+                <div class="contenedorImg">
+                    <img class=portrait src="images/redDeadRedemptionIcon.jpeg" alt="">
+                    <p id=nombreJuego>Red Dead Redemption 2</p>
+                    <div class="row precioCarro">
+                        <div class="col-6 precio">
+                          <h4>$2560,<span style="font-size:15px;">90</span></h4>
+                        </div>
+                        <div class="col-6 carro">
+                          <i class="ion-android-cart"></i>
+                        </div>
+                    </div>
+                </div>
+              </div>
+
+              <div class="col-6 dos">
+                <div class="contenedorImg">
+                    <img class=portrait src="images/spiderManIcon.jpg" alt="">
+                    <p id=nombreJuego>Spider Man</p>
+                    <div class="row precioCarro">
+                        <div class="col-6 precio">
+                          <h4>$2560,<span style="font-size:15px;">90</span></h4>
+                        </div>
+                        <div class="col-6 carro">
+                          <i class="ion-android-cart"></i>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+
+
+        <div class="carousel-item">
+          <div class="row portadasEstrenos">
+            <div class="col-6 uno">
+              <div class="contenedorImg">
+                  <img class=portrait src="images/Soulcalibur6.jpg" alt="Second slide">
+                  <p id=nombreJuego>Soulcalibur 6</p>
+                  <div class="row precioCarro">
+                      <div class="col-6 precio">
+                        <h4>$2560,<span style="font-size:15px;">90</span></h4>
+                      </div>
+                      <div class="col-6 carro">
+                        <i class="ion-android-cart"></i>
+                      </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-6 dos">
+              <div class="contenedorImg">
+                  <img class=portrait src="images/MetroExodus.jpg" alt="Second slide">
+                  <p id=nombreJuego>Metro Exodus</p>
+                  <div class="row precioCarro">
+                      <div class="col-6 precio">
+                        <h4>$2560,<span style="font-size:15px;">90</span></h4>
+                      </div>
+                      <div class="col-6 carro">
+                        <i class="ion-android-cart"></i>
+                      </div>
+                  </div>
+              </div>
+            </div>
+
+          </div>
+
+
+        </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row ">
+    <div class="col-12 separador2">
+
+    </div>
+  </div>
+</div>
