9cf11b324a3b1c6cec109f571b66778255790178
SantiagoPazos925 <santiagoPazos925@gmail.com>
2018-11-15 11:34:11 -0300

    cambios header

--- TpFinal/header.php
+++ TpFinal/header.php
@@ -2,38 +2,37 @@
 
 
 <div class="row navBar">
-  <div class="col-6  logo">
+  <div class="col-4  logo">
     <h2><a style="text-decoration:none" href="index.php"><span style='color:rgb(203, 51, 42);'>Digital</span><span style="color:white;">games</span></h2></a>
   </div>
-  <div class="col-6 iconos">
+  <div class="col-6  iconos">
 
     <i class="ion-android-cart"></i>
     <i class="ion-navicon-round"></i>
   </div>
-  <div class="col-6  categorias">
+  <div class="col-6   categorias">
+      <?php
+        $categoria = [
+          ["cate"=>"Home","url"=>"index.php", "col"=>"  col-2"],
+          ["cate"=>"Preguntas Frecuentes","url"=>"faq.php", "col"=>"col-4"],
+          ["cate"=>"Registro","url"=>"registro.php", "col"=>"col-2"],
+          ["cate"=>"Login","url"=>"login.php", "col"=>"col-2"],
+          ["cate"=>"Usuario","url"=>"miPerfil.php", "col"=>"col-1"]
 
-      <ul class="row col-12 navigation" style="text-align: center;">
-        <div id="navText" class="col-2">
-          <a id="navText" href="index.php"><li >Home</li></a>
-        </div>
-        <div  id="navText" class="col-4">
-          <a id="navText" href="faq.php"><li >Preguntas Frecuentes</li></a>
-        </div>
-        <div  id="navText" class="col-2">
-
-          <a id="navText" href="registro.php"><li >Registro</li></a>
-        </div>
-        <div  id="navText" class="col-2">
-          <a id="navText" href="login.php"><li >Login</li></a>
-        </div>
-        <div class="col-2 carrito">
+        ];
+       ?>
+      <ul class="row  navigation" style="text-align: center;">
+        <?php foreach ($categoria as $cat): ?>
+          <div id="navText" class="<?php echo $cat["col"] ?>">
+            <a id="navText" href="<?php echo $cat["url"] ?>"><li ><?php echo $cat["cate"] ?></li></a>
+          </div>
+        <?php endforeach; ?>
+          <div class="col-2 col-lg-2 carrito">
 
           <i class="ion-android-cart"></i>
 
         </div>
       </ul>
-
-
   </div>
   </div>
   <div id="search" class="container">
